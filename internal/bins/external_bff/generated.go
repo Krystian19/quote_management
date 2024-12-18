8dc700d3e90d8137c410b9ebec6251a3e4130050
Jan Guzman <janfrancisco19@gmail.com>
2024-12-18 13:34:13 -0400

    missing generated files

--- internal/bins/external_bff/generated.go
+++ internal/bins/external_bff/generated.go
@@ -51,7 +51,8 @@ type DirectiveRoot struct {
 
 type ComplexityRoot struct {
 	Mutation struct {
-		CreateTax func(childComplexity int, fields CreateTaxInput) int
+		CreateTax   func(childComplexity int, fields CreateTaxInput) int
+		CreateTaxes func(childComplexity int, fields []*CreateTaxInput) int
 	}
 
 	Query struct {
@@ -79,6 +80,7 @@ type ComplexityRoot struct {
 
 type MutationResolver interface {
 	CreateTax(ctx context.Context, fields CreateTaxInput) (*db.Tax, error)
+	CreateTaxes(ctx context.Context, fields []*CreateTaxInput) (bool, error)
 }
 type QueryResolver interface {
 	GetQuote(ctx context.Context, id uuid.UUID) (*db.Quote, error)
@@ -124,6 +126,18 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 		}
 
 		return e.complexity.Mutation.CreateTax(childComplexity, args["fields"].(CreateTaxInput)), true
+
+	case "Mutation.createTaxes":
+		if e.complexity.Mutation.CreateTaxes == nil {
+			break
+		}
+
+		args, err := ec.field_Mutation_createTaxes_args(context.TODO(), rawArgs)
+		if err != nil {
+			return 0, false
+		}
+
+		return e.complexity.Mutation.CreateTaxes(childComplexity, args["fields"].([]*CreateTaxInput)), true
 
 	case "Query.getAllTaxes":
 		if e.complexity.Query.GetAllTaxes == nil {
@@ -363,6 +377,21 @@ func (ec *executionContext) field_Mutation_createTax_args(ctx context.Context, r
 	return args, nil
 }
 
+func (ec *executionContext) field_Mutation_createTaxes_args(ctx context.Context, rawArgs map[string]interface{}) (map[string]interface{}, error) {
+	var err error
+	args := map[string]interface{}{}
+	var arg0 []*CreateTaxInput
+	if tmp, ok := rawArgs["fields"]; ok {
+		ctx := graphql.WithPathContext(ctx, graphql.NewPathWithField("fields"))
+		arg0, err = ec.unmarshalNCreateTaxInput2ᚕᚖgithubᚗcomᚋKrystian19ᚋquote_managementᚋinternalᚋbinsᚋexternal_bffᚐCreateTaxInputᚄ(ctx, tmp)
+		if err != nil {
+			return nil, err
+		}
+	}
+	args["fields"] = arg0
+	return args, nil
+}
+
 func (ec *executionContext) field_Query___type_args(ctx context.Context, rawArgs map[string]interface{}) (map[string]interface{}, error) {
 	var err error
 	args := map[string]interface{}{}
@@ -494,6 +523,61 @@ func (ec *executionContext) fieldContext_Mutation_createTax(ctx context.Context,
 	}()
 	ctx = graphql.WithFieldContext(ctx, fc)
 	if fc.Args, err = ec.field_Mutation_createTax_args(ctx, field.ArgumentMap(ec.Variables)); err != nil {
+		ec.Error(ctx, err)
+		return fc, err
+	}
+	return fc, nil
+}
+
+func (ec *executionContext) _Mutation_createTaxes(ctx context.Context, field graphql.CollectedField) (ret graphql.Marshaler) {
+	fc, err := ec.fieldContext_Mutation_createTaxes(ctx, field)
+	if err != nil {
+		return graphql.Null
+	}
+	ctx = graphql.WithFieldContext(ctx, fc)
+	defer func() {
+		if r := recover(); r != nil {
+			ec.Error(ctx, ec.Recover(ctx, r))
+			ret = graphql.Null
+		}
+	}()
+	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
+		ctx = rctx // use context from middleware stack in children
+		return ec.resolvers.Mutation().CreateTaxes(rctx, fc.Args["fields"].([]*CreateTaxInput))
+	})
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	if resTmp == nil {
+		if !graphql.HasFieldError(ctx, fc) {
+			ec.Errorf(ctx, "must not be null")
+		}
+		return graphql.Null
+	}
+	res := resTmp.(bool)
+	fc.Result = res
+	return ec.marshalNBoolean2bool(ctx, field.Selections, res)
+}
+
+func (ec *executionContext) fieldContext_Mutation_createTaxes(ctx context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
+	fc = &graphql.FieldContext{
+		Object:     "Mutation",
+		Field:      field,
+		IsMethod:   true,
+		IsResolver: true,
+		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
+			return nil, errors.New("field of type Boolean does not have child fields")
+		},
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			err = ec.Recover(ctx, r)
+			ec.Error(ctx, err)
+		}
+	}()
+	ctx = graphql.WithFieldContext(ctx, fc)
+	if fc.Args, err = ec.field_Mutation_createTaxes_args(ctx, field.ArgumentMap(ec.Variables)); err != nil {
 		ec.Error(ctx, err)
 		return fc, err
 	}
@@ -3076,6 +3160,13 @@ func (ec *executionContext) _Mutation(ctx context.Context, sel ast.SelectionSet)
 			if out.Values[i] == graphql.Null {
 				out.Invalids++
 			}
+		case "createTaxes":
+			out.Values[i] = ec.OperationContext.RootResolverMiddleware(innerCtx, func(ctx context.Context) (res graphql.Marshaler) {
+				return ec._Mutation_createTaxes(ctx, field)
+			})
+			if out.Values[i] == graphql.Null {
+				out.Invalids++
+			}
 		default:
 			panic("unknown field " + strconv.Quote(field.Name))
 		}
@@ -3812,6 +3903,28 @@ func (ec *executionContext) marshalNBoolean2bool(ctx context.Context, sel ast.Se
 func (ec *executionContext) unmarshalNCreateTaxInput2githubᚗcomᚋKrystian19ᚋquote_managementᚋinternalᚋbinsᚋexternal_bffᚐCreateTaxInput(ctx context.Context, v interface{}) (CreateTaxInput, error) {
 	res, err := ec.unmarshalInputCreateTaxInput(ctx, v)
 	return res, graphql.ErrorOnPath(ctx, err)
+}
+
+func (ec *executionContext) unmarshalNCreateTaxInput2ᚕᚖgithubᚗcomᚋKrystian19ᚋquote_managementᚋinternalᚋbinsᚋexternal_bffᚐCreateTaxInputᚄ(ctx context.Context, v interface{}) ([]*CreateTaxInput, error) {
+	var vSlice []interface{}
+	if v != nil {
+		vSlice = graphql.CoerceList(v)
+	}
+	var err error
+	res := make([]*CreateTaxInput, len(vSlice))
+	for i := range vSlice {
+		ctx := graphql.WithPathContext(ctx, graphql.NewPathWithIndex(i))
+		res[i], err = ec.unmarshalNCreateTaxInput2ᚖgithubᚗcomᚋKrystian19ᚋquote_managementᚋinternalᚋbinsᚋexternal_bffᚐCreateTaxInput(ctx, vSlice[i])
+		if err != nil {
+			return nil, err
+		}
+	}
+	return res, nil
+}
+
+func (ec *executionContext) unmarshalNCreateTaxInput2ᚖgithubᚗcomᚋKrystian19ᚋquote_managementᚋinternalᚋbinsᚋexternal_bffᚐCreateTaxInput(ctx context.Context, v interface{}) (*CreateTaxInput, error) {
+	res, err := ec.unmarshalInputCreateTaxInput(ctx, v)
+	return &res, graphql.ErrorOnPath(ctx, err)
 }
 
 func (ec *executionContext) unmarshalNFloat2float64(ctx context.Context, v interface{}) (float64, error) {
